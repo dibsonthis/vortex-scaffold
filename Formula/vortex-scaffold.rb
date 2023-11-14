f08c0c6d11ec43bdc2d99972a2d6dc4085977a12
Adib Attie <adib@Adib-Atties-Mac.local>
2023-11-15 09:56:00 +1100

    fix

--- Formula/vortex-scaffold.rb
+++ Formula/vortex-scaffold.rb
@@ -1,19 +1,19 @@
 class VortexScaffold < Formula
-    desc "A self-contained repository for Vortex programs"
-    homepage "https://github.com/dibsonthis/vortex-scaffold"
-    url "https://github.com/dibsonthis/vortex-scaffold/assets/vortex-dist-1.0.tar.gz"
-    sha256 "ddd75f9fa9342a041d9ff5c537f6607662c9f97f"
-  
-    def install
-      bin.install "vortex"
-      prefix.install "modules"
-      prefix.install "config.ini"
-      prefix.install "run"
-      prefix.install "src"
-    end
-  
-    test do
-      # Add some test if needed
-      system "true"
-    end
+  desc "A self-contained repository for Vortex programs"
+  homepage "https://github.com/dibsonthis/vortex-scaffold"
+  url "https://github.com/dibsonthis/vortex-scaffold/blob/main/assets/vortex-dist-1.0.tar.gz"
+  sha256 "ddd75f9fa9342a041d9ff5c537f6607662c9f97f"
+
+  def install
+    bin.install "vortex"
+    prefix.install "modules"
+    prefix.install "config.ini"
+    prefix.install "run"
+    prefix.install "src"
   end
+
+  test do
+    # Add some test if needed
+    system "true"
+  end
+end
