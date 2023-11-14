9c1d7ad4deb09050ae791ecf7fa50ae0442cbd7b
Adib Attie <adib@Adib-Atties-Mac.local>
2023-11-15 09:39:01 +1100

    init

---
+++ vortex-scaffold.rb
@@ -0,0 +1,21 @@
+class VortexScaffold < Formula
+    desc "A self-contained repository for Vortex programs"
+    homepage "https://github.com/dibsonthis/vortex-scaffold"
+    url "https://github.com/dibsonthis/vortex-scaffold/archive/1.0.0.tar.gz"
+    sha256 "ddd75f9fa9342a041d9ff5c537f6607662c9f97f"
+  
+    depends_on "vortex" # If vortex is a dependency
+  
+    def install
+      bin.install "vortex"
+      prefix.install "modules"
+      prefix.install "config.ini"
+      prefix.install "run"
+      prefix.install "src"
+    end
+  
+    test do
+      # Add some test if needed
+      system "true"
+    end
+  end
