5560917779566f1a264706fc597f8e62d248267c
Adib Attie <adib@Adib-Atties-Mac.local>
2023-11-15 09:56:45 +1100

    fix

--- Formula/vortex-scaffold.rb
+++ Formula/vortex-scaffold.rb
@@ -2,7 +2,7 @@ class VortexScaffold < Formula
   desc "A self-contained repository for Vortex programs"
   homepage "https://github.com/dibsonthis/vortex-scaffold"
   url "https://github.com/dibsonthis/vortex-scaffold/blob/main/assets/vortex-dist-1.0.tar.gz"
-  sha256 "ddd75f9fa9342a041d9ff5c537f6607662c9f97f"
+  sha256 "dd2d706acdccec7790497af4ba8b577cd15c158ed0d4824e2cdeb585de0342f5"
 
   def install
     bin.install "vortex"
